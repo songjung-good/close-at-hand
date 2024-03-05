7870fbe0ff21dc646d192f31a46ef10eb01d7a71
최진우 <chjnw0204@gmail.com>
2024-03-05 22:37:37 +0900

    Revert "commit test 2"
    This reverts commit 942fa7896c8cddb5e040169c39b7675b88a5aadd

---
+++ backend/CloseAtHand/src/main/java/com/rainbowmirror/closeathand/CloseAtHandApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowmirror.closeathand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloseAtHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseAtHandApplication.class, args);
+	}
+
+}
--- backend/close-at-hand/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "close-at-hand"
