da7b80a8b3fdd2b7617f35962a0e238e7d5cd748
최도훈 <chlehgns123@naver.com>
2024-03-08 17:39:23 +0900

    chore: tanstackquery provider 설정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,27 +1,13 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./src/shared/lib/tanstackquery/tanstackQuery";
 
-import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, View } from "react-native";
-import { useOtherNameStore, usePersonStore } from "./src/app/store/store";
+import Index from "./src/app/example/Index";
 
 function App() {
-	const { firstName, updateFirstName } = usePersonStore();
-	const { name, updateName } = useOtherNameStore();
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text>앱을 시작하려면 App.js를 열어보세요!</Text>
-			<StatusBar style="auto" />
-			<Text>{firstName}</Text>
-			<TextInput
-				className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-				onChangeText={(text) => updateFirstName(text)}
-			></TextInput>
-			<Text>{name.firstName}</Text>
-			<TextInput
-				className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-				onChangeText={(text) => updateName(text)}
-			></TextInput>
-		</View>
+		<QueryClientProvider client={queryClient}>
+			<Index />
+		</QueryClientProvider>
 	);
 }
 
