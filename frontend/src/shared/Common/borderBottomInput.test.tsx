a90c345ed53f85076da4242423bad734a915daeb
최도훈 <chlehgns123@naver.com>
2024-03-12 09:17:23 +0900

    test: 공통 컴포넌트 BorderBottomInput 테스트코드 작성
    Change-Id: If6404c52d6e21006e81c74531f2f3e980ae2fbd6

---
+++ frontend/src/shared/Common/borderBottomInput.test.tsx
@@ -0,0 +1,63 @@
+import "@testing-library/jest-native/";
+import { render, fireEvent } from "@testing-library/react-native";
+import renderer from "react-test-renderer";
+
+import BorderBottomInput from "./BorderBottomInput";
+
+describe("BorderBottomInput", () => {
+	const INITIAL_VALUE = "value";
+	const TestID = "input";
+
+	// 스냅샷 테스트 코드의 내용을 변경한다면 __snapshots__에서 해당 파일 삭제
+	it("스냅샷 테스트", () => {
+		const tree = renderer
+			.create(
+				<BorderBottomInput
+					value={INITIAL_VALUE}
+					setValue={jest.fn()}
+					placeholder="플레이스홀더"
+				/>,
+			)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("placeholder가 작성됩니다.", () => {
+		const { getByPlaceholderText } = render(
+			<BorderBottomInput
+				value={INITIAL_VALUE}
+				setValue={jest.fn()}
+				placeholder="플레이스홀더"
+			/>,
+		);
+		expect(getByPlaceholderText("플레이스홀더")).toBeTruthy();
+	});
+
+	it("value를 렌더링합니다.", () => {
+		const { getByTestId } = render(
+			<BorderBottomInput
+				value={INITIAL_VALUE}
+				setValue={jest.fn()}
+				placeholder="플레이스홀더"
+			/>,
+		);
+		expect(getByTestId(TestID)).toBeTruthy();
+	});
+
+	it("텍스트를 입력하면 setValue가 입력값을 받아 실행됩니다.", () => {
+		const setValue = jest.fn();
+		const { getByTestId } = render(
+			<BorderBottomInput
+				value={INITIAL_VALUE}
+				setValue={setValue}
+				placeholder="플레이스홀더"
+			/>,
+		);
+
+		const input = getByTestId(TestID);
+
+		fireEvent.changeText(input, "새 입력");
+
+		expect(setValue).toHaveBeenCalledWith("새 입력");
+	});
+});
