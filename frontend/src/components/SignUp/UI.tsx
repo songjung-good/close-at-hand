d4ce75ca95240e1687603b77bb9fb0ac6656460e
최도훈 <chlehgns123@naver.com>
2024-03-13 10:46:17 +0900

    feat: ErrorText UI 추가
    Change-Id: I45bcdf63c2df67daeb9dc75e6047372e29f8949f

---
+++ frontend/src/components/SignUp/UI.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text } from "react-native";
+import { COLORS } from "../../shared";
+
+interface Props {
+	children: ReactNode;
+}
+
+export const ErrorText: React.FC<Props> = ({ children }) => {
+	return <Text style={styles.text}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+	text: {
+		color: COLORS.CarrotRed,
+		textAlign: "center",
+	},
+});
