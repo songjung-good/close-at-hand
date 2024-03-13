a9a00b49f1f3df3e0884b9d94cbca7ae057a97b5
YH <139380475+songjung-good@users.noreply.github.com>
2024-03-13 17:42:43 +0900

    Feat: 블루투스 기능
    블루투스 기능에 대한 예시코드

--- frontend/src/screens/home/ControllerScreen.tsx
+++ frontend/src/screens/home/ControllerScreen.tsx
@@ -1,9 +1,43 @@
 // src/screens/home/ControllerScreen.tsx
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+// import { Bluetooth, PermissionStatus, requestBluetoothPermission } from 'expo-bluetooth';
 
 const ControllerScreen: React.FC = () => {
+  // const [bluetoothState, setBluetoothState] = useState<PermissionStatus | null>(null);
+
+  // useEffect(() => {
+  //   // 컴포넌트가 마운트되면 블루투스 상태를 확인
+  //   checkBluetoothState();
+
+  //   // 언마운트 시에 정리(clean-up) 함수 호출
+  //   return () => {
+  //     // 필요하다면 정리(clean-up) 작업 수행
+  //   };
+  // }, []);
+
+  // const checkBluetoothState = async () => {
+  //   // 블루투스 권한을 요청
+  //   const permissionStatus = await requestBluetoothPermission();
+    
+  //   // 블루투스 상태를 가져옴
+  //   const bluetoothStatus = await Bluetooth.getStatusAsync();
+    
+  //   setBluetoothState(bluetoothStatus);
+  // };
+
+  // const handleButtonPress = async (deviceFunction: string) => {
+  //   // 블루투스 상태 확인
+  //   if (bluetoothState?.isConnected) {
+  //     // 블루투스가 연결된 상태이면 요청 보내기
+  //     console.log(`Clicked button to control ${deviceFunction}`);
+  //     // 여기서 블루투스 기기에 대한 요청을 보내는 로직을 추가하세요.
+  //   } else {
+  //     console.log('블루투스가 연결되어 있지 않습니다.');
+  //   }
+  // };
+  
   const handleButtonPress = (deviceFunction: string) => {
     // 여기서 버튼 클릭에 대한 로직을 작성하면 됩니다.
     console.log(`Clicked button to control ${deviceFunction}`);
