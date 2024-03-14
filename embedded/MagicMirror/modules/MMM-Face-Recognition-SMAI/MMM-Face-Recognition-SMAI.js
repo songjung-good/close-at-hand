4ff1035ab826dcd6cf42e4c8a8ac457ca01fb8d7
RosaDamascena <hsj990604@gmail.com>
2024-03-14 18:02:21 +0900

    feat: 얼굴인식 로그인 시 효과음 추가
    Change-Id: Ice9ecb284e7998e836a89713dd764f2c309c7952

--- embedded/MagicMirror/modules/MMM-Face-Recognition-SMAI/MMM-Face-Recognition-SMAI.js
+++ embedded/MagicMirror/modules/MMM-Face-Recognition-SMAI/MMM-Face-Recognition-SMAI.js
@@ -4,6 +4,7 @@ Module.register("MMM-Face-Recognition-SMAI", {
   defaults: {
     prompt: "Use FaceID to access profiles",
     fileUrl: "modules/MMM-Face-Recognition-SMAI/public/face1.png",
+    soundUrl: "modules/MMM-Face-Recognition-SMAI/public/FaceID_success.mp3",
     width: "300px",
     position: "left",
     refreshInterval: 2
@@ -11,6 +12,8 @@ Module.register("MMM-Face-Recognition-SMAI", {
 
   start: function () {
     this.count = 0
+    this.isLoggedIn = false
+    this.sound = new Audio(this.config.soundUrl)
   },
 
   getStyles: function () {
@@ -48,37 +51,50 @@ Module.register("MMM-Face-Recognition-SMAI", {
   socketNotificationReceived: function(notification, payload) {
       switch(notification) {
         case "I_DID":
-      //Store Image Here
-
-        var elem = document.getElementById("COUNT")
-        elem.innerHTML = "Welcome back, " + payload
-
-        //Creat Image Element Image
-      elem.classList.add(this.config.position);
-      elem.style.width = this.config.width;
-      
-
-      var img = document.createElement("img");
-      img.setAttribute('src', this.config.fileUrl);
-      elem.appendChild(img);
-      return elem
-        
-      break;
+          if (!this.isLoggedIn) {
+            // 비로그인 상태에서 로그인 상태로 변경될 때
+  
+            // 사용자 환영 메시지 업데이트
+            var elem = document.getElementById("COUNT");
+            elem.innerHTML = "Welcome back, " + payload;
+            elem.classList.add(this.config.position);
+            elem.style.width = this.config.width;
+  
+            // 이미지 업데이트
+            var img = document.createElement("img");
+            img.setAttribute('src', this.config.fileUrl);
+            elem.appendChild(img);
+  
+            // 효과음 재생
+            this.sound.play();
+  
+            // 로그인 상태로 설정
+            this.isLoggedIn = true;
+  
+            this.updateDom();
+          }
+          break;
       
       default:
-        var elem = document.getElementById("COUNT")
-        elem.innerHTML = "Welcome back, User"
+        if (this.isLoggedIn) {
+          this.isLoggedIn = false; 
+          
+          var elem = document.getElementById("COUNT")
+          elem.innerHTML = "Welcome back, User"
 
           //Creat Image Element Image
-      elem.classList.add(this.config.position);
-      elem.style.width = this.config.width;
-      
+        elem.classList.add(this.config.position);
+        elem.style.width = this.config.width;
+        
+        
 
-      var img = document.createElement("img");
-      img.setAttribute('src', "modules/MMM-Face-Recognition-SMAI/public/guest.gif");
-      elem.appendChild(img);
-      return elem
+        var img = document.createElement("img");
+        img.setAttribute('src', "modules/MMM-Face-Recognition-SMAI/public/guest.gif");
+        elem.appendChild(img);
+        this.updateDom();
+      }
+      // return elem
+      break;
     }
   },
-
 })
