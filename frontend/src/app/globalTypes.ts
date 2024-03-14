6327f601734a55f49895bb7829f297e9c8b283a4
최도훈 <chlehgns123@naver.com>
2024-03-14 12:15:30 +0900

    feat: 글로벌 네비게이션 타입 설정 및 블루투스 연결화면 이미지 추가
    Change-Id: I0b680032b16c2dd3baa09b2591135d27e8919715

--- frontend/src/app/globalTypes.ts
+++ frontend/src/app/globalTypes.ts
@@ -1,12 +1,25 @@
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RouteProp } from "@react-navigation/native";
 
-import type { RootParamList } from "./Navigation/AppNav";
-import type { HomeParamList } from "./Navigation/HomeNav";
-import type { ClosetParamList } from "./Navigation/ClosetNav";
-import type { MangeMentParamList } from "./Navigation/ManagementNav";
-import type { SettingsParamList } from "./Navigation/SettingsNav";
-import type { OnBoardingParamList } from "./Navigation/OnBoardingNav";
+import type { RootParamList } from "./navigation/AppNav";
+import type { HomeParamList } from "./navigation/HomeNav";
+import type { ClosetParamList } from "./navigation/ClosetNav";
+import type { MangeMentParamList } from "./navigation/ManagementNav";
+import type { SettingsParamList } from "./navigation/SettingsNav";
+import type { OnBoardingParamList } from "./navigation/OnBoardingNav";
+
+type NativeStackParamList = RootParamList &
+	HomeParamList &
+	ClosetParamList &
+	MangeMentParamList &
+	SettingsParamList &
+	OnBoardingParamList;
+
+type RootScreenType = {
+	[key in keyof NativeStackParamList]: NativeStackParamList[key];
+};
+
 declare global {
 	interface RootNav extends BottomTabNavigationProp<RootParamList> {}
 	interface HomeNav extends NativeStackNavigationProp<HomeParamList> {}
@@ -16,4 +29,8 @@ declare global {
 	interface SettingsNav extends NativeStackNavigationProp<SettingsParamList> {}
 	interface OnBoardingNav
 		extends NativeStackNavigationProp<OnBoardingParamList> {}
+	interface RootScreenProps {
+		navigation: NativeStackNavigationProp<RootScreenType>;
+		route: RouteProp<RootScreenType>;
+	}
 }
