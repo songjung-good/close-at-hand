cdc26c3591667e1f6ae15f4973aaf13538700070
최도훈 <chlehgns123@naver.com>
2024-03-14 17:58:39 +0900

    fix; 버그 발생으로 라이브러리 삭제 ㅣㅁㅊ 안드로이드 코드 초기화
    Change-Id: Id1b09fba01255786ccec3237b3b19f64c0881881

--- frontend/src/screens/onboarding/BluetoothConnection.tsx
+++ frontend/src/screens/onboarding/BluetoothConnection.tsx
@@ -1,17 +1,24 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import { StyledButton } from "../../shared";
+import { COLORS } from "../../app";
 
 const BluetoothConnection: React.FC<NavigationProps> = ({ navigation }) => {
 	function hanldeCancel() {
 		navigation.navigate("0");
 	}
 	return (
-		<View>
-			<Text>주변의 기기를 검색 중입니다.</Text>
-			<Image
-				source={require("../../../assets/image/bluetoothSearching.png")}
-			></Image>
-			<Text>스마트 미러를 켜주시고 연결될 때까지 잠시 기다려 주세요</Text>
+		<View style={styles.container}>
+			<Text style={[styles.title, styles.textCenter]}>
+				주변의 기기를 검색 중입니다.
+			</Text>
+			<View style={styles.imageContainer}>
+				<Image
+					source={require("../../../assets/image/bluetoothSearching.png")}
+				></Image>
+			</View>
+			<Text style={[styles.text, styles.textCenter]}>
+				스마트 미러를 켜주시고 연결될 때까지 잠시 기다려 주세요
+			</Text>
 			<StyledButton
 				title="취소"
 				onPress={hanldeCancel}
@@ -23,4 +30,25 @@ const BluetoothConnection: React.FC<NavigationProps> = ({ navigation }) => {
 
 export default BluetoothConnection;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+	},
+	title: {
+		color: COLORS.Turquoise,
+		fontSize: 32,
+		marginVertical: 4,
+	},
+	imageContainer: {
+		alignItems: "center",
+		marginVertical: 10,
+	},
+	text: {
+		fontSize: 16,
+		marginVertical: 5,
+	},
+	textCenter: {
+		textAlign: "center",
+	},
+});
