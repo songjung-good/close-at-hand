7b7ebb350883b704d987810a38ef3d64207e9d9b
최진우 <chjnw0204@gmail.com>
2024-03-14 17:40:43 +0900

    feat: add cors config

--- backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/user/UserInfo.java
+++ backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/user/UserInfo.java
@@ -6,7 +6,7 @@ import lombok.ToString;
 @Getter
 @ToString
 public class UserInfo {
-    private final Long user_id;
+    private final Long userId;
     private final String userToken;
     private final String userName;
     private final Float height;
@@ -14,7 +14,7 @@ public class UserInfo {
     private final User.Status status;
 
     public UserInfo(User user) {
-        this.user_id = user.getUserId();
+        this.userId = user.getUserId();
         this.userToken = user.getUserToken();
         this.userName = user.getUserName();
         this.height = user.getHeight();
