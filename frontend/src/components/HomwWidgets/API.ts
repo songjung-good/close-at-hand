148a2f47cfaf85e81dac8107a7ffc417b6a6b532
최도훈 <chlehgns123@naver.com>
2024-03-21 16:12:28 +0900

    fix: console Warning Possible unhandled promise rejection 경고는 catch의에서 에러를 throw하여 발생, 하지만 tanstack query에서 에러 핸들링하려면 어쩔 수 없는 듯..
    Change-Id: I27574099ef175b830e03a1d9f6d2fb5f9a73fbb5

--- frontend/src/components/HomwWidgets/API.ts
+++ frontend/src/components/HomwWidgets/API.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { API } from "../../shared";
 
 interface FetchTodayInterface {
@@ -14,23 +15,17 @@ interface TodayResponse {}
 export async function fetchToday({
 	signal,
 }: FetchTodayInterface): Promise<NoResponse | TodayResponse> {
-	return API.get("주소", { signal })
-		.then((response) => {
-			if (response.status === 204) {
-				return {
-					message:
-						"기록된 오늘의 코디가 없어요! \n 오늘의 코디를 추가해 주세요.",
-					noResponse: true,
-				} as NoResponse;
-			} else {
-				return response.data as TodayResponse;
-			}
-		})
-		.catch((error) => {
-			if (error.response) {
-				throw new Error(error.response.data?.message ?? "네트워크 에러");
-			} else {
-				throw new Error("인터넷 연결을 확인해주세요.");
-			}
-		});
+	try {
+		const response = await API.get("주소", { signal });
+		if (response.status === 204) {
+			return {
+				message: "기록된 오늘의 코디가 없어요! \n 오늘의 코디를 추가해 주세요.",
+				noResponse: true,
+			} as NoResponse;
+		} else {
+			return response.data as TodayResponse;
+		}
+	} catch (error) {
+		throw new Error((error as AxiosError).message ?? "네트워크 에러");
+	}
 }
