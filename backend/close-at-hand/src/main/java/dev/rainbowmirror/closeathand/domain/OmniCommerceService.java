a7d4fe0a8b5863518a1f1dc0b76048bafe9b522d
최진우 <chjnw0204@gmail.com>
2024-03-22 10:39:17 +0900

    docs: private

--- backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/OmniCommerceService.java
+++ backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/OmniCommerceService.java
@@ -1,32 +1,19 @@
 package dev.rainbowmirror.closeathand.domain;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import dev.rainbowmirror.closeathand.interfaces.clothes.OmniBodyDto;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class OmniCommerceService {
 
-//    @Value("${}")
-    private String XApiKey="test-2XPioSKYZtt4MzHXH3QGoB938xTCbqxqChHT9Z8L";
-    private String url = "https://api.kr.omnicommerce.ai/2022-08/management/products";
+    @Value("${omni.X-Api-Key}")
+    private String XApiKey;
+
+    @Value("${omni.url}")
+    private String url;
 
 
     public int postClothes(String id, String clothesImgUrl, String userToken){
