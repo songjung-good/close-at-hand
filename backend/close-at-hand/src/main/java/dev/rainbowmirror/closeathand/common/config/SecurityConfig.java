61c1b2e4fe721bbb8904ffa89dada69351c067ce
최진우 <chjnw0204@gmail.com>
2024-03-26 17:15:34 +0900

    feat: config security for swagger docs url pass
    Change-Id: Ia9f4f1e90092e603d535ee76998b59db087546b5

--- backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/common/config/SecurityConfig.java
+++ backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/common/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -73,9 +74,9 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login/**", "/", "/error",
+                        .requestMatchers("/login/**", "/", "/error", "/swagger-config/**", "/api-docs/**",
                                 "/swagger/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
-                        .requestMatchers("/user").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/user").permitAll()
                         .anyRequest().authenticated());
         http
                 .addFilterBefore(new JWTFilter(jwtUtil,userReader), LoginFilter.class);
