a40090b34f749c3763942d5c6b8ecbed788630ec
최진우 <chjnw0204@gmail.com>
2024-03-28 16:10:56 +0900

    feat: 많이 입은 옷 통계 쿼리
    Change-Id: I9e6fcccc1bfe10f53a155b5dfeb7b229a378567e

--- backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/preset/PresetDetail.java
+++ backend/close-at-hand/src/main/java/dev/rainbowmirror/closeathand/domain/preset/PresetDetail.java
@@ -1,2 +1,25 @@
-package dev.rainbowmirror.closeathand.domain.preset;public class PresetDetail {
+package dev.rainbowmirror.closeathand.domain.preset;
+
+import dev.rainbowmirror.closeathand.common.AbstractEntity;
+import dev.rainbowmirror.closeathand.domain.clothes.Clothes;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "preset_detail")
+public class PresetDetail extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long preset_detail_id;
+
+    @ManyToOne
+    private Preset preset;
+
+    @ManyToOne
+    private Clothes clothes;
 }
